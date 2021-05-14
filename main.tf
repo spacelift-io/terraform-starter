764c466359125741be5cef3086d09a1f66a57abe
Jacob Martin <kubam@spacelift.io>
2021-05-14 12:08:14 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     spacelift = {
-      source = "spacelift.io/spacelift-io/spacelift"
+      source = "downloads.spacelift.dev/spacelift-io/spacelift"
     }
   }
 }
