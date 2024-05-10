97d39cd95bfa0243261dc62439c8b4c1bc92da03
jdrodriguezrui <julianruizr1@gmail.com>
2024-05-10 14:18:53 -0500

    Remove poc bucket

--- bucket.tf
+++
@@ -1,7 +0,0 @@
-resource "google_storage_bucket" "test" {
-  name          = "julian-spacelift-poc-test"
-  location      = "europe-west1"
-  project       = "development-env-ta"
-  storage_class = "REGIONAL"
-  force_destroy = true
-}
