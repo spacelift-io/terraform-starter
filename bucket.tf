42c668e50d605a364ff4772877a30516095e39f5
jdrodriguezrui <julianruizr1@gmail.com>
2024-05-10 14:19:29 -0500

    Add commented out bucket

---
+++ bucket.tf
@@ -0,0 +1,7 @@
+#resource "google_storage_bucket" "test" {
+#  name          = "julian-spacelift-poc-test"
+#location      = "europe-west1"
+#  project       = "development-env-ta"
+#  storage_class = "REGIONAL"
+#  force_destroy = true
+#}
