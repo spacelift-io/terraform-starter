8cd5244513b804b0710bab61e104de1808ab2f7e
Gaurav Prachchhak <gaurav.prachchhak@prenuvo.com>
2024-09-20 13:52:17 -0700

    test

--- context.tf
+++ context.tf
@@ -1,18 +1,21 @@
 locals {
-  context_data = yamldecode(file("${path.module}/contexts/platform-dev.yaml"))
+  context_data = fileset("${path.module}/contexts", "*.yaml")
 }
 resource "spacelift_context" "managed" {
-  name        = local.context_data.name
-  description = local.context_data.description
-  labels      = local.context_data.labels
+
+  for_each = local.context_data
+
+  name        = yamldecode(file(each.value)).name
+  description = yamldecode(file(each.value)).description
+  labels      = yamldecode(file(each.value)).labels
 }
 
-resource "spacelift_environment_variable" "context-plaintext" {
+# resource "spacelift_environment_variable" "context-plaintext" {
 
-  for_each = local.context_data.variables
+#   for_each = local.context_data
 
-  context_id = spacelift_context.managed.id
-  name       = each.key
-  value      = each.value
-  write_only = false
-}
+#   context_id = spacelift_context.managed.id
+#   name       = each.key
+#   value      = each.value
+#   write_only = false
+# }
