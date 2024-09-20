2037026ec9984346fa5dd27115e5194057540bcb
Gaurav Prachchhak <gaurav.prachchhak@prenuvo.com>
2024-09-20 14:44:55 -0700

    testing

--- context.tf
+++ context.tf
@@ -5,23 +5,23 @@ locals {
   ]
 }
 resource "spacelift_context" "managed" {
+  for_each = { for context in local.contexts_data : context.name => context }
 
-  count = length(local.contexts_data)
-
-  name        = local.contexts_data[count.index]["name"]
-  description = local.contexts_data[count.index]["description"]
-  labels      = local.contexts_data[count.index]["labels"]
+  name        = each.value["name"]
+  description = each.value["description"]
+  labels      = each.value["labels"]
 }
+
 
 resource "spacelift_environment_variable" "context-plaintext" {
-  for_each = { 
-    for context in local.contexts_data : 
-    "${context.name}-${context.variables["env"]}" => context.variables 
-    if context.variables != null 
+  for_each = {
+    for context in local.contexts_data : context.name => context.variables
+    if context.variables != null
   }
 
-  context_id = spacelift_context[each.key].managed.id
-  name       = each.value["name"]
-  value      = each.value
+  context_id = spacelift_context[each.key].id
+  name       = each.key
+  value      = each.value["env"]
   write_only = false
 }
+
