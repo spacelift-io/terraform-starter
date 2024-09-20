775f728d1cbd6a80535ff836225f6c7156bd628b
Gaurav Prachchhak <gaurav.prachchhak@prenuvo.com>
2024-09-20 12:26:58 -0700

    deleted creation of new stack

--- stack.tf
+++ stack.tf
@@ -1,13 +1,13 @@
-data "spacelift_current_stack" "this" {}
+# data "spacelift_current_stack" "this" {}
 
-resource "spacelift_stack" "root_admin_stack" {
-  name        = "Root Admin Stack"
-  description = "Root Admin Stack for CloudOps used for managing spaces and contexts. DO NOT DELETE."
+# resource "spacelift_stack" "root_admin_stack" {
+#   name        = "Root Admin Stack"
+#   description = "Root Admin Stack for CloudOps used for managing spaces and contexts. DO NOT DELETE."
 
-  repository   = "cloudops-spacelift-admin-stack"
-  branch       = "main"
-  project_root = "managed-stack"
+#   repository   = "cloudops-spacelift-admin-stack"
+#   branch       = "main"
+#   project_root = "managed-stack"
 
-  autodeploy = true
-  labels     = ["managed", "depends-on:${data.spacelift_current_stack.this.id}"]
-}
+#   autodeploy = true
+#   labels     = ["managed", "depends-on:${data.spacelift_current_stack.this.id}"]
+# }
