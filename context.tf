99877ff8208aabe0f75ef356c951627d477b0638
Gaurav Prachchhak <gaurav.prachchhak@prenuvo.com>
2024-09-20 15:44:49 -0700

    testing modified context

--- context.tf
+++ context.tf
@@ -1,18 +1,41 @@
+# locals {
+#   context_data = yamldecode(file("${path.module}/contexts/platform-dev.yaml"))
+# }
+# resource "spacelift_context" "managed" {
+#   name        = local.context_data.name
+#   description = local.context_data.description
+#   labels      = local.context_data.labels
+# }
+
+# resource "spacelift_environment_variable" "context-plaintext" {
+
+#   for_each = local.context_data.variables
+
+#   context_id = spacelift_context.managed.id
+#   name       = each.key
+#   value      = each.value
+#   write_only = false
+# }
+
 locals {
-  context_data = yamldecode(file("${path.module}/contexts/platform-dev.yaml"))
+  yaml_files = fileset("${path.module}/contexts", "*.yaml")
+
+  context_data_list = [for file in local.yaml_files : yamldecode(file("${path.module}/contexts/${file}"))]
 }
+
 resource "spacelift_context" "managed" {
-  name        = local.context_data.name
-  description = local.context_data.description
-  labels      = local.context_data.labels
+  for_each = { for i, context in local.context_data_list : i => context }
+
+  name        = each.value.name
+  description = each.value.description
+  labels      = each.value.labels
 }
 
 resource "spacelift_environment_variable" "context-plaintext" {
+  for_each = { for context in local.context_data_list : context.name => context.variables }
 
-  for_each = local.context_data.variables
-
-  context_id = spacelift_context.managed.id
-  name       = each.key
-  value      = each.value
+  context_id = spacelift_context.managed[each.key].id
+  name       = each.value.key
+  value      = each.value.value
   write_only = false
 }
