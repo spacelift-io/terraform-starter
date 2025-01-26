8f89cadc53138ce4044e5672cb3fa80f13a449b6
Keny Debo <kenydebo@gmail.com>
2025-01-25 22:18:29 -0500

    Adding vulnerable Azure Storage

--- managed-stack/main.tf
+++ managed-stack/main.tf
@@ -4,3 +4,13 @@ resource "random_password" "secret" {
   length  = 29
   special = true
 }
+
+resource "azurerm_storage_account" "public" {
+  name                     = "publicstorageaccount"
+  resource_group_name      = "example-resources"
+  location                 = "West Europe"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  allow_nested_items_to_be_public = true
+}
