395b303da1f589167c779b63427b2c03e71d4454
Curtis Hodak <curtish@spacelift.com>
2025-06-30 12:05:46 -0400

    change name back

--- stack.tf
+++ stack.tf
@@ -1,7 +1,7 @@
 data "spacelift_current_stack" "this" {}
 
 resource "spacelift_stack" "managed" {
-  name        = "managed-slack-reslug"
+  name        = "managed-stack"
   description = "Your first stack managed by Terraform"
 
   repository   = "terraform-starter"
